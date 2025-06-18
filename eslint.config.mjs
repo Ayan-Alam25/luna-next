0c0570031a3bf4730423d32c6d5ab1e56fa8304f
Ayan Alam <ayanalam0025@gmail.com>
2025-06-18 15:43:04 +0530

    fix: deployment error

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,16 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "react/no-unescaped-entities": [
+        "error",
+        {
+          forbid: [">", "}", '"'], // Only forbid these characters, allow apostrophes
+        },
+      ],
+    },
+  },
 ];
 
 export default eslintConfig;
